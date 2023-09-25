6c28901a523f1913031bc8afbe253daf1e728e56
shelton <damwcs@gmail.com>
2023-09-25 14:32:44 +1000

    fix bugs

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,9 +1,11 @@
 import MainContent from "@/components/MainContent";
+import {post} from "@/types/tpyes";
 
 function Page() {
+  const posts:post[] = [{title:"title1",postContent:"content1"},{title:"title2",postContent:"content2"}]
   return (
     <div>
-      <MainContent title="New Draft" type="edit"></MainContent>
+      <MainContent title="New Draft" type="edit" posts={posts}></MainContent>
     </div>
   );
 }
