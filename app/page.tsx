f2791cc89dd476fd5046b5baea40959701678a90
shelton <damwcs@gmail.com>
2023-09-25 14:38:40 +1000

    fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image'
 import Post from "@/components/Post";
 import PostList from "@/components/PostList";
 import MainContent from "@/components/MainContent";
+import {post} from "@/types/tpyes";
 
 export default function FeedPage() {
+  const posts:post[] = [{title:"title1",postContent:"content1"},{title:"title2",postContent:"content2"}]
   return (
-    <MainContent title="Public Feed" type="read"></MainContent>
+    <MainContent title="Public Feed" type="read" posts={posts}></MainContent>
   )
 }
